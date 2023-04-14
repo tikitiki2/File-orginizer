7e5531f90cddc8b813681ad57483a0995af73833
tikitiki2 <ethanyitzhaky@gmail.com>
2023-04-14 13:38:30 -0400

    added new options

--- orginize.py
+++ orginize.py
@@ -2,49 +2,49 @@
 import os
 from depend import funcs as f
 
-
+# focus on creating the program through commandline first then create the GUI no need to create gui until done make
 
 
 #file type or extension
 filetype_ext=input('enter the kind of file you would like to organize or enter a specific file extension such as .mp3: ').lower()
-#calls funcs dependancy where validation also happens
+
 ftype=f.get_file_type(filetype_ext) # returns tuple with 2 elements if starts with . and 3 if not
 #example output ('category', 'images', ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.svg'])
 
 directory=input('enter full directory of where to cleanup enter nothing to use current directory: ')
-
+# ask for permission to enter files in directory and look into those files recursively
+check_files=input('go into all files in directory Y/N: ').lower()  # lower for consistency
+if check_files=='y':
+    check_files = True
+    try:
+        limit=int(input('enter depth limit enter nothing for no limit: '))
+    except Exception as e:
+        print(e)
+        exit(1)
+    if not limit:
+        limit=None
+else:
+    check_files=False
+    limit = None
 if not directory:
     #default directory
     dir_path = os.path.dirname(os.path.realpath(__file__))
     if not os.path.isdir(dir_path):
          print(f'invalid directory {directory}')
          exit(1)
-    elif ftype[0]=='extension':
-        # is a file extension
-        f.move(dir_path=dir_path,file_type=ftype[1],extension=filetype_ext)
-        pass
-    elif ftype[0]=='category':
-        #is a category of files
-        f.move(dir_path=dir_path,file_type=ftype[1],li=ftype[2])
-        pass
     else:
-        print('unkown error, how did you get here this is not possible')
-        exit(1)
+        f.move(dir_path=dir_path, file_info=ftype,check_files=check_files,limit=limit)
 else:
     if not os.path.isdir(directory):
          print(f'invalid directory {directory}')
          exit(1)
-    elif ftype[0]=='extension':
-        # is a file extension
-        f.move(dir_path=directory,file_type=ftype[1],extension=filetype_ext)
-        pass
-    elif ftype[0]=='category':
-        #is a category of files
-        f.move(dir_path=directory,file_type=ftype[1],li=ftype[2])
-        pass
     else:
-        print('unkown error, how did you get here this is not possible')
-        exit(1)
+        f.move(dir_path=directory,file_info=ftype,check_files=check_files,limit=limit)
+
+#todo upload to github
+
+
+
 
 
 
