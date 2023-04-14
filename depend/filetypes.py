e44ce99d444329ca3910dcc8a357b464431a654f
tikitiki2 <ethanyitzhaky@gmail.com>
2023-04-14 13:39:33 -0400

    added ability to recursivly go throguh folders

--- depend/filetypes.py
+++ depend/filetypes.py
@@ -1,12 +1,11 @@
 extensions = {
     'image': [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".svg"],
     'audio': [".mp3", ".wav", ".wma", ".aac", ".flac", ".m4a", ".ogg"],
-    'document': [".pdf", ".doc", ".docx", ".ppt", ".pptx", ".xls", ".xlsx", ".csv", ".txt", ".rtf"],
-    'video': [".mp4", ".mov", ".avi", ".wmv", ".flv", ".mkv"],
+    'document': [".pdf", ".doc", ".docx", ".ppt", ".pptx", ".xls", ".xlsx", ".csv", ".txt", ".rtf",".xps"],
+    'video': [".mp4", ".mov", ".avi", ".wmv", ".flv", ".mkv",".WebM"],
     'code': [".py", ".java", ".c", ".cpp", ".h", ".html", ".css", ".js", ".php", ".rb", ".pl", ".sh"]
 }
 
 #list of possible extension categories
 types = ["images",'image', "audio",'audios', "documents",'document', "video",'videos', "code",'codes']
-
 
--- depend/funcs.py
+++ depend/funcs.py
@@ -1,29 +1,58 @@
 import shutil
 import os
 from depend import filetypes as ft
-#pass in directory to clean up and file extension type
-def move(dir_path,file_type=None,extension=None,li=None):
+global count
+count=0
 
-    #check if folder exists
-    if extension:
-        if not os.path.isdir(dir_path + f'\\{file_type[1::]}'):
-            os.mkdir(dir_path + f'\\{file_type[1::]}')
-        for file in os.scandir(dir_path):
-            file_name = file.name
-            if file_name.endswith(extension):
-                print(file_name)
-                shutil.move(os.path.join(dir_path, file_name), os.path.join(dir_path, file_type[1:]))
+def move(dir_path, file_info ,check_files=False,limit=None):
+    #file_info= ('category', 'images', ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.svg'])
+    if not os.path.isdir(dir_path+f'\\{file_info[1]}'):
+        os.mkdir(dir_path+f'\\{file_info[1]}')
+
+    new_dir=dir_path+f'\\{file_info[1]}'
+    if file_info[0]=='category':
+        blackbox(dir_path,file_info[2],new_dir,check_files,limit=limit)
     else:
-        if not os.path.isdir(dir_path + f'\\{file_type}'):
-            os.mkdir(dir_path + f'\\{file_type}')
-        for file in os.scandir(dir_path):
-            file_name = file.name
-            ext=file_name[file_name.rfind('.') + 1:]
+        blackbox(dir_path,[file_info[1]],new_dir,check_files,limit=limit)
 
-            if '.'+ext in li:
-                print(file_name)
-                shutil.move(os.path.join(dir_path, file_name), os.path.join(dir_path, file_type))
+def blackbox(full_path,ext,move_to,check_file=False,limit=None,cur_depth=0):
+    if limit!=None:
+        if cur_depth>=limit:
+            print(f'reached max depth at {full_path}')
+            return
+    for file in os.scandir(full_path):
+        if file.name.endswith(tuple(ext)):
+            print(file.name)
+            if os.path.exists(f'{move_to}\\{file.name}'): # https://docs.python.org/3/library/os.path.html?highlight=exists#os.path.exists
+                print(f'duplicate file found {file.name}')
+                new_name=rename(file.path)
+                os.rename(file.path,new_name)
 
+                shutil.move(new_name, move_to)
+            else:
+                shutil.move(file.path,move_to)
+        elif check_file:
+            if file.is_dir(follow_symlinks=True):
+                print(f'directory found --> {file.name}')
+                blackbox(file.path, ext, move_to, check_file,limit=limit,cur_depth=cur_depth+1)
+
+
+#rename duplicate files
+def rename(file_path):
+    global count
+    ext=''
+    index=len(file_path)
+    for i in range(len(file_path)-1,0,-1):
+        if file_path[i]=='.':
+            ext=file_path[i::]
+            index=i
+        elif file_path[i]=='\\':
+            path=file_path[0:i+1]
+            fname=file_path[i+1:index]
+            break
+    count+=1
+    new_path=path+fname+f'({count})'+ext
+    return new_path
 
 def get_file_type(filetype_ext):
     if not filetype_ext:
